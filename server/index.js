24cddd0d7d7062880cd86b85d06ba7801613c04d
Artem <ashikunec@gmail.com>
2025-04-06 07:08:50 +0300

    fit: add db, .env files

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
+require('dotenv').config()
+const express = require('express')
 
+const PORT = process.env.PORT || 5001
+
+const app = express()
+
+app.listen(PORT,() => console.log(`Server was starting at port ${PORT}`))
