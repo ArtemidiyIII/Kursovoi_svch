0daf9fc175e6b1b9dc461f32fdf57c1681d6bb7a
Artem <ashikunec@gmail.com>
2025-04-13 22:09:12 +0300

    fit: add models connections

--- server/models/models.js
+++ server/models/models.js
@@ -52,8 +52,61 @@ const Rafting = sequelize.define('rafting', {
     img: {type: DataTypes.STRING, allowNull: false, },
 })
 
+const River = sequelize.define('river', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,}, 
+    name: {type: DataTypes.STRING, unique:true, allowNull: false, },   
+})
+
 const RaftingRating = sequelize.define('rafting_rating', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true,},  
     rate: {type: DataTypes.INTEGER, allowNull: false, },  
 })
 
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+Basket.hasMany(BasketRent)
+BasketRent.belongsTo(Basket)
+
+Basket.hasMany(BasketRafting)
+BasketRafting.belongsTo(Basket)
+
+Type.hasMany(RentedItem)
+RentedItem.belongsTo(Type)
+
+Brand.hasMany(RentedItem)
+RentedItem.belongsTo(Brand)
+
+RentedItem.hasMany(BasketRent)
+BasketRent.belongsTo(RentedItem)
+
+RentedItem.hasMany(RentInfo)
+RentInfo.belongsTo(RentedItem)
+
+Rafting.hasMany(BasketRafting)
+BasketRafting.belongsTo(Rafting)
+
+River.hasMany(Rafting)
+Rafting.belongsTo(River)
+
+User.hasMany(RaftingRating)
+RaftingRating.belongsTo(User)
+
+Rafting.hasMany(RaftingRating)
+RaftingRating.belongsTo(Rafting)
+
+
+module.exports ={
+    User,
+    Basket,
+    BasketRent,
+    BasketRafting,
+    RentedItem,
+    Type,
+    Brand,
+    RentInfo,
+    River,
+    Rafting,
+    RaftingRating
+}
