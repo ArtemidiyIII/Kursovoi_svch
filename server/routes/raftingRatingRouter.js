1e9f542732058acb2cb74c8caf155938b689c45c
Artem <ashikunec@gmail.com>
2025-04-24 07:58:35 +0300

    fit: add bcrypt and jsonwebtoken modules

--- server/routes/raftingRatingRouter.js
+++ server/routes/raftingRatingRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express')
 const router = new Router()
-const raftingRaitingController = require('../controllers/raftingRaitingController')
+const raftingRatingController = require('../controllers/raftingRatingController')
 const checkrole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', checkrole('USER','ADMIN'), raftingRaitingController.create)
-router.get('/', checkrole('USER','ADMIN'), raftingRaitingController.getAll)
-router.get('/:raftingId', checkrole('USER','ADMIN'), raftingRaitingController.getByRaftingId)
-router.put('/:raftingId', checkrole('USER','ADMIN'), typeController.updateRaftingRaiting)
+router.post('/', checkrole('USER','ADMIN'), raftingRatingController.create)
+router.get('/', checkrole('USER','ADMIN'), raftingRatingController.getAll)
+router.get('/:raftingId', checkrole('USER','ADMIN'), raftingRatingController.getByRaftingId)
+router.put('/:raftingId', checkrole('USER','ADMIN'), raftingRatingController.updateRaftingRaiting)
 
 module.exports = router
