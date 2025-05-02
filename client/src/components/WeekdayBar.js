6af33dfbdb4790a4ade3ac7c562f62f0092f2654
Artem <ashikunec@gmail.com>
2025-05-03 00:34:59 +0300

    fit: add WeekdayBar.js for Rafting Catalog

--- client/src/components/WeekdayBar.js
+++ client/src/components/WeekdayBar.js
@@ -1,11 +1,14 @@
-import React  from "react";
+import React, { useContext }  from "react";
+import {observer} from "mobx-react-lite";
+import {Context} from '..';
 
-const WeekDayBar = () => {
+const WeekDayBar = observer(() => {
+    const {raftings} = useContext(Context)
     return (
         <div>
 
         </div>
     )
-}
+})
 
 export default WeekDayBar;
