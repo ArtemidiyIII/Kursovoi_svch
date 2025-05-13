c72a06aa23cee58ad9f95e75486261b0ed2d5748
Artem <ashikunec@gmail.com>
2025-05-13 11:17:57 +0300

    fit: add modal windows for create and update raftings and rent items

--- client/src/components/modals/CreateRafting.js
+++ client/src/components/modals/CreateRafting.js
@@ -2,21 +2,22 @@ import React, {useEffect, useContext, useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
 import Modal from 'react-bootstrap/esm/Modal';
 import { Col, Dropdown, Form, Row } from 'react-bootstrap';
-import { Context } from '../..';
-import { fetchTypes, fetchBrands, createGood} from '../../http/goodAPI';
+import { Context } from '../../index';
+import { fetchRivers, fetchWeekdays, createRafting} from '../../http/raftingAPI';
 import { observer } from 'mobx-react-lite';
 
     
-const CreateGood= observer( ({show,onHide}) => {
-    const {goods} = useContext(Context)
+const CreateRafting = observer( ({show,onHide}) => {
+    const {raftings} = useContext(Context)
     const [name, setName]=useState('')
     const [price, setPrice]=useState(0)
+    const [discount_price, setDiscountPrice] = useState(0)
     const [file, setFile]=useState(null)    
     const [info , setInfo]=useState([])
 
 useEffect(() => {
-    fetchTypes().then(data=>goods.setTypes(data))
-    fetchBrands().then(data=>goods.setBrands(data))
+    fetchRivers().then(data=>raftings.setRivers(data))
+    fetchWeekdays().then(data=>raftings.setWeekdays(data))
     }, [])
 
     const selectFile=e=>{
@@ -32,30 +33,31 @@ useEffect(() => {
     const removeInfo=(number)=>{
         setInfo(info.filter(i=> i.number!== number))
     }
-    const addGood = () => {
+    const addRafting = () => {
         const formData = new FormData();
         formData.append('name', name);
         formData.append('price', `${price}`);
+        formData.append('discount_price', `${discount_price}`);
         formData.append('img', file);
-        formData.append('brandId', goods.selectedBrand.id);
-        formData.append('typeId', goods.selectedType.id);
+        formData.append('weekdayId', raftings.selectedWeekday.id);
+        formData.append('riverId', raftings.selectedRiver.id);
     
         
         const updatedInfo = info.map(i => ({
             ...i,
-            goodId: null 
+            raftingId: null 
         }));
     
         formData.append('info', JSON.stringify(updatedInfo));
     
-        createGood(formData).then(data => {
+        createRafting(formData).then(data => {
            
-            const goodId = data.id; 
-            const updatedInfoWithGoodId = updatedInfo.map(i => ({
+            const raftingId = data.id; 
+            const updatedInfoWithRaftingId = updatedInfo.map(i => ({
                 ...i,
-                goodId: goodId
+                raftingId: raftingId
             }));
-            setInfo(updatedInfoWithGoodId);  
+            setInfo(updatedInfoWithRaftingId);  
               onHide();
         });
     };
@@ -70,81 +72,87 @@ useEffect(() => {
       
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add new Product
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+                Добавить новый сплав на байдарке
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Dropdown className='mt-2'>
+                    <Dropdown.Toggle>{raftings.selectedRiver.name || "Выбрать реку"}</Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {raftings.rivers.map(river=>
+                            <Dropdown.Item onClick={()=>raftings.setSelectedRiver(river)} key={river.id}>
+                                {river.name}
+                            </Dropdown.Item>
+                        )}
+                    </Dropdown.Menu>
+                </Dropdown>
             <Dropdown className='mt-2'>
-                <Dropdown.Toggle>{goods.selectedType.name || "Choose type"}</Dropdown.Toggle>
+                <Dropdown.Toggle>{raftings.selectedWeekday.name || "Выбрать день"}</Dropdown.Toggle>
                 <Dropdown.Menu>
-                    {goods.types.map(type=>
-                        <Dropdown.Item onClick={()=>goods.setSelectedType(type)} key={type.id}>
-                            {type.name}
+                    {raftings.weekdays.map(weekday=>
+                        <Dropdown.Item onClick={()=>raftings.setSelectedWeekday(weekday)} key={weekday.id}>
+                            {weekday.name}
                         </Dropdown.Item>
                     )}
                 </Dropdown.Menu>
             </Dropdown>
-            <Dropdown className='mt-2'>
-                <Dropdown.Toggle>{goods.selectedBrand.name || "Choose brand"}</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {goods.brands.map(brand=>
-                        <Dropdown.Item onClick={()=>goods.setSelectedBrand(brand)} key={brand.id}>
-                            {brand.name}
-                        </Dropdown.Item>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
             <Form.Control
-            value={name}
-                    onChange={e=>setName(e.target.value)}
-            className='mt-2'
-            placeholder='name of product'/>
+                value={name}
+                onChange={e=>setName(e.target.value)}
+                className='mt-2'
+                placeholder='название сплава'/>
             <Form.Control
-             value={price}
-                    onChange={e=>setPrice(Number(e.target.value))}
-            className='mt-2'
-            placeholder='price of product'
-            type="number"/>
+                value={price}
+                onChange={e=>setPrice(Number(e.target.value))}
+                className='mt-2'
+                placeholder='цена сплава'
+                type="number"/>
             <Form.Control
-            className='mt-2'
-            placeholder='image of product'
-            onChange={selectFile}
-            type="file"/>
+                value={discount_price}
+                onChange={e=>setDiscountPrice(Number(e.target.value))}
+                className='mt-2'
+                placeholder='цена по скидке'
+                type="number"/>
+            <Form.Control
+                className='mt-2'
+                placeholder='фотография сплава'
+                onChange={selectFile}
+                type="file"/>
             <hr></hr>
             <Button variant='ouline-dark'
-            onClick={addInfo}>Add new property</Button>
+            onClick={addInfo}>Добавить дополнительную информацию</Button>
             {info.map(i=>
                 <Row className='mt-2' key={i.number}>
                     <Col md={4}>
                     <Form.Control
-                    value={i.title}
-                    onChange={(e)=>changeInfo('title', e.target.value, i.number)}
-                        placeholder='title'
+                        value={i.title}
+                        onChange={(e)=>changeInfo('title', e.target.value, i.number)}
+                        placeholder='заголовок'
                     />
                     </Col>
                     <Col md={4}>
                     <Form.Control
-                     value={i.description}
-                     onChange={(e)=>changeInfo('description', e.target.value, i.number)}
-                        placeholder='description'
+                        value={i.description}
+                        onChange={(e)=>changeInfo('description', e.target.value, i.number)}
+                        placeholder='описание'
                     />
                     </Col>
                     <Col md={4}>
-                        <Button variant='outline-danger' onClick={()=>removeInfo(i.number)}>Delete</Button>
+                        <Button variant='outline-danger' onClick={()=>removeInfo(i.number)}>Удалить</Button>
                     </Col>
                 </Row>
             )}
         </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant='outline-danger' onClick={addGood}>Add</Button>
-        <Button variant='outline-success' onClick={onHide}>Close</Button>
-      </Modal.Footer>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant='outline-danger' onClick={addRafting}>Добавить</Button>
+            <Button variant='outline-success' onClick={onHide}>Закрыть</Button>
+        </Modal.Footer>
     </Modal>
   );
 })
 
-export default CreateGood;
+export default CreateRafting;
--- client/src/components/modals/CreateRentItem.js
+++ client/src/components/modals/CreateRentItem.js
@@ -2,21 +2,21 @@ import React, {useEffect, useContext, useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
 import Modal from 'react-bootstrap/esm/Modal';
 import { Col, Dropdown, Form, Row } from 'react-bootstrap';
-import { Context } from '../..';
-import { fetchTypes, fetchBrands, createGood} from '../../http/goodAPI';
+import { Context } from '../../index';
+import { fetchTypes, fetchBrands, createRentedItem} from '../../http/rentItemAPI';
 import { observer } from 'mobx-react-lite';
 
     
-const CreateGood= observer( ({show,onHide}) => {
-    const {goods} = useContext(Context)
+const СreateRentedItem = observer( ({show,onHide}) => {
+    const {rented_items} = useContext(Context)
     const [name, setName]=useState('')
     const [price, setPrice]=useState(0)
     const [file, setFile]=useState(null)    
     const [info , setInfo]=useState([])
 
 useEffect(() => {
-    fetchTypes().then(data=>goods.setTypes(data))
-    fetchBrands().then(data=>goods.setBrands(data))
+    fetchTypes().then(data=>rented_items.setTypes(data))
+    fetchBrands().then(data=>rented_items.setBrands(data))
     }, [])
 
     const selectFile=e=>{
@@ -32,119 +32,118 @@ useEffect(() => {
     const removeInfo=(number)=>{
         setInfo(info.filter(i=> i.number!== number))
     }
-    const addGood = () => {
+    const addRentItem = () => {
         const formData = new FormData();
         formData.append('name', name);
         formData.append('price', `${price}`);
         formData.append('img', file);
-        formData.append('brandId', goods.selectedBrand.id);
-        formData.append('typeId', goods.selectedType.id);
+        formData.append('brandId', rented_items.selectedBrand.id);
+        formData.append('typeId', rented_items.selectedType.id);
     
         
         const updatedInfo = info.map(i => ({
             ...i,
-            goodId: null 
+            rented_itemId: null 
         }));
     
         formData.append('info', JSON.stringify(updatedInfo));
     
-        createGood(formData).then(data => {
+        createRentedItem(formData).then(data => {
            
-            const goodId = data.id; 
-            const updatedInfoWithGoodId = updatedInfo.map(i => ({
+            const rented_itemId = data.id; 
+            const updatedInfoWithRentItemId = updatedInfo.map(i => ({
                 ...i,
-                goodId: goodId
+                rented_itemId: rented_itemId
             }));
-            setInfo(updatedInfoWithGoodId);  
+            setInfo(updatedInfoWithRentItemId);  
               onHide();
         });
     };
     
         
 
-  return (
-    <Modal
-      show={show}
-      onHide={onHide}
-      size="lg"
-      
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add new Product
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-            <Dropdown className='mt-2'>
-                <Dropdown.Toggle>{goods.selectedType.name || "Choose type"}</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {goods.types.map(type=>
-                        <Dropdown.Item onClick={()=>goods.setSelectedType(type)} key={type.id}>
-                            {type.name}
-                        </Dropdown.Item>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown className='mt-2'>
-                <Dropdown.Toggle>{goods.selectedBrand.name || "Choose brand"}</Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {goods.brands.map(brand=>
-                        <Dropdown.Item onClick={()=>goods.setSelectedBrand(brand)} key={brand.id}>
-                            {brand.name}
-                        </Dropdown.Item>
-                    )}
-                </Dropdown.Menu>
-            </Dropdown>
-            <Form.Control
-            value={name}
-                    onChange={e=>setName(e.target.value)}
-            className='mt-2'
-            placeholder='name of product'/>
-            <Form.Control
-             value={price}
-                    onChange={e=>setPrice(Number(e.target.value))}
-            className='mt-2'
-            placeholder='price of product'
-            type="number"/>
-            <Form.Control
-            className='mt-2'
-            placeholder='image of product'
-            onChange={selectFile}
-            type="file"/>
-            <hr></hr>
-            <Button variant='ouline-dark'
-            onClick={addInfo}>Add new property</Button>
-            {info.map(i=>
-                <Row className='mt-2' key={i.number}>
-                    <Col md={4}>
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"      
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить новое снаряжение
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Dropdown className='mt-2'>
+                        <Dropdown.Toggle>{rented_items.selectedType.name || "Выбрать тип снаряжения"}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {rented_items.types.map(type=>
+                                <Dropdown.Item onClick={()=>rented_items.setSelectedType(type)} key={type.id}>
+                                    {type.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown className='mt-2'>
+                        <Dropdown.Toggle>{rented_items.selectedBrand.name || "Выбрать бренд снаряжения"}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {rented_items.brands.map(brand=>
+                                <Dropdown.Item onClick={()=>rented_items.setSelectedBrand(brand)} key={brand.id}>
+                                    {brand.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <Form.Control
-                    value={i.title}
-                    onChange={(e)=>changeInfo('title', e.target.value, i.number)}
-                        placeholder='title'
-                    />
-                    </Col>
-                    <Col md={4}>
+                        value={name}
+                        onChange={e=>setName(e.target.value)}
+                        className='mt-2'
+                        placeholder='название снаряжения'/>
                     <Form.Control
-                     value={i.description}
-                     onChange={(e)=>changeInfo('description', e.target.value, i.number)}
-                        placeholder='description'
-                    />
-                    </Col>
-                    <Col md={4}>
-                        <Button variant='outline-danger' onClick={()=>removeInfo(i.number)}>Delete</Button>
-                    </Col>
-                </Row>
-            )}
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant='outline-danger' onClick={addGood}>Add</Button>
-        <Button variant='outline-success' onClick={onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+                        value={price}
+                        onChange={e=>setPrice(Number(e.target.value))}
+                        className='mt-2'
+                        placeholder='цена проката снаряжения'
+                        type="number"/>
+                    <Form.Control
+                        className='mt-2'
+                        placeholder='фото снаряжения'
+                        onChange={selectFile}
+                        type="file"/>
+                    <hr></hr>
+                    <Button variant='ouline-dark'
+                    onClick={addInfo}>Добавить дополнительную информацию</Button>
+                    {info.map(i=>
+                        <Row className='mt-2' key={i.number}>
+                            <Col md={4}>
+                            <Form.Control
+                                value={i.title}
+                                onChange={(e)=>changeInfo('title', e.target.value, i.number)}
+                                placeholder='заголовок'
+                            />
+                            </Col>
+                            <Col md={4}>
+                                <Form.Control
+                                    value={i.description}
+                                    onChange={(e)=>changeInfo('description', e.target.value, i.number)}
+                                    placeholder='описание'
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Button variant='outline-danger' onClick={()=>removeInfo(i.number)}>Удалить</Button>
+                            </Col>
+                        </Row>
+                    )}
+                </Form>
+            </Modal.Body>
+        <Modal.Footer>
+            <Button variant='outline-danger' onClick={addRentItem}>Добавить</Button>
+            <Button variant='outline-success' onClick={onHide}>Закрыть</Button>
+        </Modal.Footer>
+        </Modal>
+    );
 })
 
-export default CreateGood;
+export default СreateRentedItem;
