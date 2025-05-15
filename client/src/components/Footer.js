23fb0be46db11547ebaae425c408624bced2e283
Artem <ashikunec@gmail.com>
2025-05-15 13:48:39 +0300

    fit: add footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import Nav from 'react-bootstrap/Nav';
+import { PROFILE_ROUTE, HOME_ROUTE } from '../utils/consts';
+
+const Footer = () => {
+  return (
+    <div style={{}}>
+        <footer style={{position:'fixed',bottom: 0, left: 0, width: '100%', backgroundColor: '#343a40' }}>
+            
+        </footer>
+    </div>
+  );
+}
+
+export default Footer;
