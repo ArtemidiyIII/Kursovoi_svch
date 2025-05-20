39bf755bfefa48d2b3ae84f181bca3b9566f3387
Artem <ashikunec@gmail.com>
2025-05-20 06:24:00 +0300

    fit: add user profile page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import { RAFTINGCATALOG_ROUTE, RENTCATALOG_ROUTE, FAQ_ROUTE } from '../utils/con
 const Footer = () => {
     return (
         <div style={{}}>
-            <footer style={{position:'fixed',bottom: 15, left: 0, width: '100%', backgroundColor: '#343a40' }}>
+            <footer style={{position:'fixed',bottom: 0, left: 0, width: '100%', backgroundColor: '#343a40' }}>
                 <Nav className="justify-content-center" activeKey="/home">
                     <Nav.Item>
                         <Nav.Link href={RAFTINGCATALOG_ROUTE} style={{color:'white'}}>Сплавы</Nav.Link>
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -2,16 +2,50 @@ import React, { useContext, useState, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Context } from '../../src/index';
 import {  useNavigate } from 'react-router-dom';
-
+import {RAFTINGCATALOG_ROUTE, RENTCATALOG_ROUTE, ORDERSRAFTING_ROUTE, ORDERSRENT_ROUTE, ADMIN_ROUTE} from '../utils/consts'
+import { Container } from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 const UserProfile = observer(() => {
     const { user } = useContext(Context); 
     const navigate = useNavigate();
 
 
     return (
-        <div>
-
-        </div>
+        <Container size="lg" className='mt-5 d-flex justify-content-center align-items-center'>
+            <Card  style={{ borderRadius: '10px' }}>
+                <Row className='mb-3 mt-2 '>
+                    <Col className='d-flex'>
+                        <Button className='py-3 mx-2 w-100' variant="secondary" onClick={() => navigate(RAFTINGCATALOG_ROUTE)}>Забронировать сплав</Button>
+                    
+                        <Button className='py-3 mx-2 w-100' variant="secondary" onClick={() => navigate(RENTCATALOG_ROUTE)}>Прокат снаряжения</Button>
+                    </Col>
+                </Row>
+                <Row className='mb-3'>
+                    <Col className='mx-2'>
+                        <h2>Информация о пользователе</h2>
+                        <p>Email: {user.user.email}</p>
+                        <p>Role: {user.user.role}</p>
+                    </Col>
+                </Row>
+                <Row className='mb-2'>
+                    <Col className='d-flex'>
+                        <Button className='py-3 mx-2 w-100' variant="secondary" onClick={() => navigate(ORDERSRAFTING_ROUTE)}>Мои сплавы</Button>
+                    
+                        <Button className='py-3 mx-2 w-100' variant="secondary" onClick={() => navigate(ORDERSRENT_ROUTE)}>Мой список снаряжения</Button>
+                    </Col>
+                </Row>
+                {user.user.role === 'ADMIN' && (      
+                    <Row className='mb-2 mt-1'>
+                        <Col className='d-flex'>
+                            <Button className='py-3 mx-2 w-100' variant="secondary" onClick={() => navigate(ADMIN_ROUTE)}>Админ-панель</Button>                    
+                        </Col>
+                    </Row>
+                )}
+            </Card>
+        </Container>        
     )
 
 })
